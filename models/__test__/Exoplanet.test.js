c8dec84232491b8a9b7b16740c3e49e00c43f094
sasha_tour <sashavanrossum9@gmail.com>
2022-12-22 17:16:08 +0100

    test: test that the unique name doesn't contains special charactere that aren't allowed

--- models/__test__/Exoplanet.test.js
+++ models/__test__/Exoplanet.test.js
@@ -1,3 +1,15 @@
+const exoplanets = require('../../models/Exoplanet');
+
 test('pipeline-test', () => {
     expect(0).toBe(0);
+});
+
+test('no special charactere are allowed', () => {
+    expect(
+        exoplanets.save({
+            uniqueName: 'MADA./-',
+            hClass: 'test',
+            discoveryYear: '1000',
+        })
+    ).toBe(false);
 });
